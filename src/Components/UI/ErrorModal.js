1a8a64c0c85a6f22aab08edbf6bd6c2e59f090be
ujjwal <ujjwalsharma1772003@gmail.com>
2023-06-27 22:32:24 +0530

    added ErrorModal css

--- src/Components/UI/ErrorModal.js
+++ src/Components/UI/ErrorModal.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import Card from './Card'
 import Button from './Button'
+import classes from './ErrorModal.module.css'
 function ErrorModal(props) {
   return (
- <Card>
-     <header>
+ <Card classname={classes.modal}>
+     <header className={classes.header}>
         <h2>{props.title}</h2>
      </header>
-     <div>
+     <div className={classes.content}>
         <p>{props.message}</p>
      </div>
-     <footer>
+     <footer className={classes.actions}>
 <Button>Okay</Button>
      </footer>
  </Card>
