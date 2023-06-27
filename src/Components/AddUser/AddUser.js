b9c3f3c4d9100b2338b8bcb1588d90e3cf5bb827
ujjwal <ujjwalsharma1772003@gmail.com>
2023-06-27 22:59:51 +0530

    rendering error modal conditionally

--- src/Components/AddUser/AddUser.js
+++ src/Components/AddUser/AddUser.js
@@ -8,13 +8,22 @@ import ErrorModal  from '../UI/ErrorModal';
 function AddUser(props) {
     const [enteredUsername, setEnteredUsername] = useState('');
     const [enteredAge, setEnteredAge] = useState('');
+    const [error, setError] = useState('');
     const submitHandler = (event) => {
        event.preventDefault();
        if(enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
-        return;
+       setError({
+        title: 'Invalid Input',
+        message: 'Please enter a valid name and age(non-empty values)'
+       })
+       return;
        }
        if(+enteredAge < 1){
-        return;
+       setError({
+        title:'Invalid age',
+        message:'Please enter valid age (>0)'
+       })
+       return;
        }
        props.onAddUser(enteredUsername, enteredAge);
        setEnteredAge('');
@@ -30,7 +39,7 @@ function AddUser(props) {
     }
   return (
     <div>
-      <ErrorModal title = 'An error occured' message='Something went wrong'/>
+      {error && <ErrorModal title = {error.title} message={error.message}/>}
     <Card className={classes.input}>
    <form onSubmit={submitHandler} className='form'>
     <label htmlFor='Username'>Username</label>
